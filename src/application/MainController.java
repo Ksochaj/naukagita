3286126950c1fb07d5f1431c0b6a1ab433e43e6c
niadek <sochaj.konrad@gmail.com>
2016-03-14 01:37:11 +0100

    initial commit

---
+++ src/application/MainController.java
@@ -0,0 +1,17 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.Random;
+public class MainController {
+	@FXML
+	private Label myMessage;
+	public void generateRandom(ActionEvent event){
+		Random rand = new Random();
+		int myrand = rand.nextInt(50)+1;
+		//System.out.println(Integer.toString(myrand));
+		myMessage.setText(Integer.toString(myrand));
+	}
+}
